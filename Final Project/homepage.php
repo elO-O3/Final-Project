12d16e3081aefc50d521be9aaab85f5fd13c25b4
elO-O3 <angeloluizms3@gmail.com>
2024-11-19 22:47:29 +0800

    Add files via upload

---
+++ Final Project/homepage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Mtop.css">
+    <title>CloverWear</title>
+</head>
+<body>
+<header>
+        <div class="hpage">
+        <nav>
+            <div class="container1">
+                    <div class="logo">
+                        <img src="/d16cfa46-e1d7-4de5-8254-636272710f1a.jpg" alt="">
+                    </div>
+                </div>
+                    <p>CloverWear</p>
+                <div class="container2">
+                    <p><img id="img-1" src="user.png"></p>
+                    <p>&nbsp;</p>
+                </div>
+            </nav>
+        </div>
+        </header>
+        
+        <div class="hpagenav">
+        <nav>
+            <ul>
+                <a href="">Home</a>
+                <a href="">Shop</a>
+                <a href="">About Us</a>
+                <a href="">Buy Now</a>
+            </ul>
+        </nav>
+        </div>
+        
+        
+</body>
+</html>
